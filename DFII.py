08f898d3137473675e35b3458a0802188263fc25
hyperseb <s.looijen@tutanota.com>
2021-02-07 10:49:11 +0100

    Created structure of the project

---
+++ DFII.py
@@ -0,0 +1,8 @@
+# This scripts cleans and merges all the files in ./Sources
+# Sebastiaan Looijen, february 2021
+
+# Import libraries --------------------------------------------------------
+import numpy as np
+import pandas as pd
+
+# Read files from ./Sources -----------------------------------------------
