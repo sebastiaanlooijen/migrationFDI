e6480f28eadd1f7d61254106bc33d668ccbe9277
sebastiaanlooijen <s.looijen@tutanota.com>
2021-02-09 19:41:11 +0100

    Added cleaning countries to DFI.py

--- DFI.py
+++ DFI.py
@@ -2,7 +2,21 @@
 # Sebastiaan Looijen, february 2021
 
 # Import libraries --------------------------------------------------------
-import numpy as np
 import pandas as pd
 
 # Read files from ./Sources -----------------------------------------------
+countries_en = pd.read_csv("./Sources/countries/countries_en.csv")
+countries_nl = pd.read_csv("./Sources/countries/countries_nl.csv")
+
+# Clean countries files ---------------------------------------------------
+countries_en.rename(columns = {
+  'name': 'country_name_en',
+  'alpha2': 'country_alpha2',
+  'alpha3': 'country_alpha3'
+  }, 
+  inplace = True)
+  
+countries_nl.rename(columns = {'name': 'country_name_nl'}, inplace = True)
+countries_nl = countries_nl.drop(['alpha2', 'alpha3'], axis = 1)
+
+countries = pd.merge(countries_nl, countries_en, on = "id", how = "left")
