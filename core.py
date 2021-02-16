c1c3303903c886dd4e560b0cdc2e18b793f3bbcb
sebastiaanlooijen <s.looijen@tutanota.com>
2021-02-16 15:49:10 +0100

    Almost done merging FDI.

--- core.py
+++ core.py
@@ -12,7 +12,8 @@ countries_en.rename(
             'name': 'country_name_en',
             'alpha2': 'country_alpha2',
             'alpha3': 'country_alpha3'},
-        inplace = True)
+        inplace = True
+        )
   
 countries_nl = pd.read_csv("./Sources/Countries/countries_nl.csv") 
 countries_nl.rename(columns = {'name': 'country_name_nl'}, inplace = True)
@@ -20,12 +21,17 @@ countries_nl = countries_nl.drop(['alpha2', 'alpha3'], axis = 1)
 
 countries = pd.merge(countries_nl, countries_en, on = "id", how = "left")
 countries.rename(columns = {'id': 'country_id'}, inplace = True)
-
 countries['country_name_nl'] = countries['country_name_nl'].replace(
-  ['Micronesia',
-  'Verenigde Staten'],
-  ['Micronesië',
-  'Verenigde Staten van Amerika'])
+        [
+            'Micronesia',
+            'Verenigde Staten'
+        ],
+        [
+            'Micronesië',
+            'Verenigde Staten van Amerika'
+        ]
+        )
+countries = countries.drop(['country_id'], axis = 1)
 
 # dependent variable: immigrants
 immigrants = pd.read_csv("./Sources/Migration/migrants_nl.csv", sep = ";")
@@ -36,7 +42,8 @@ immigrants.rename(
             'LandVanHerkomstVestiging': 'Key',
             'Perioden': 'year',
             'Immigratie_1': 'immigrants'},
-        inplace = True)
+        inplace = True
+        )
   
 metadata = pd.read_csv("./Sources/Migration/metadata.csv", sep = "\t")
 immigrants = pd.merge(immigrants, metadata, on = "Key", how = "left")
@@ -47,38 +54,86 @@ immigrants = immigrants.reindex(
         columns = [
             'country_name_nl', 
             'year', 
-            'immigrants'])
+            'immigrants']
+            )
             
 immigrants['country_name_nl'] = immigrants['country_name_nl'].replace(
-  ['Bosnië-Herzegovina',
-  'Congo',
-  'Congo (Democratische Republiek)',
-  'Filippijnen',
-  'Katar',
-  'Macedonië',
-  'Marshall-eilanden',
-  'Timor Leste',
-  'Uganda'],
-  ['Bosnië en Herzegovina',
-  'Congo-Brazzaville',
-  'Congo-Kinshasa',
-  'Filipijnen',
-  'Qatar',
-  'Noord-Macedonië',
-  'Marshalleilanden',
-  'Oost-Timor',
-  'Oeganda'])            
+        [
+            'Bosnië-Herzegovina',
+            'Congo',
+            'Congo (Democratische Republiek)',
+            'Filippijnen',
+            'Katar',
+            'Macedonië',
+            'Marshall-eilanden',
+            'Timor Leste',
+            'Uganda'
+        ],
+        [
+            'Bosnië en Herzegovina',
+            'Congo-Brazzaville',
+            'Congo-Kinshasa',
+            'Filipijnen',
+            'Qatar',
+            'Noord-Macedonië',
+            'Marshalleilanden',
+            'Oost-Timor',
+            'Oeganda'
+        ]
+        )            
 
 # independent variable: FDI
 FDI = pd.read_csv("./Sources/FDI/FDI_FLOW_PARTNER.csv")
 FDI.query("FLOW == 'OUT' and CUR == 'USD' and COU == 'NLD'", inplace = True)
 FDI = FDI[['PC', 'Partner country', 'Year', 'Value']]
 FDI.rename(
-    columns = {
-        'PC': 'country_alpha3', 
-        'Partner country': 'country_name_en',
-        'Value': 'FDI'},
-    inplace = True)
+        columns = {
+            'PC': 'country_alpha3', 
+            'Partner country': 'country_name_en',
+            'Year': 'year',
+            'Value': 'FDI'},
+        inplace = True
+        )
+FDI['country_name_en'] = FDI['country_name_en'].replace(
+        [
+            'Bolivia',
+            'Czech Republic',
+            'Iran, Islamic Republic of',
+            'SriLanka',
+            'St. Vincent and the Grenadines',
+            'St. Kitts and Nevis',
+            'Timor_Leste',
+            'Venezuela'
+        ],
+        [
+            'Bolivia (Plurinational State of)',
+            'Czechia',
+            'Iran (Islamic Republic of)',
+            'Sri Lanka',
+            'Saint Vincent and the Grenadines',
+            'Saint Kitts and Nevis',
+            'Timor-Leste',
+            'Venezuela (Bolivarian Republic of)'
+        ]
+        )
+# TODO: current list is not exhaustive, needs work 
+
+FDI['key'] = FDI['country_name_en'] + FDI['year'].astype(str)
+FDI = FDI[['key', 'FDI']]
 
 # create initial data set
-core = pd.merge(immigrants, countries, on = "country_name_nl", how = "inner")
+df_1 = pd.merge(immigrants, countries, on = "country_name_nl", how = "inner")
+df_1['key'] = df_1['country_name_en'] + df_1['year']
+df_1 = df_1.reindex(
+        columns = [
+            'country_name_en',
+            'country_name_nl',
+            'country_alpha2',
+            'country_alpha3',
+            'year',
+            'key',
+            'immigrants'
+            ]
+        )
+
+df_2 = pd.merge(df_1, FDI, on = 'key', how = 'left')
